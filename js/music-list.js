4353d9a5267d3dad60d9a6ba6365daea8bd89569
Abhilaasha <129691817+Abhiilaasha@users.noreply.github.com>
2023-11-28 11:27:11 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Charlie Puth & Jungkook",
+    artist: "Jungkook ",
+    img: "music-01",
+    src: "music-01"
+  },
+  {
+    name: "Justin Bieber",
+    artist: "Audio Library",
+    img: "music-02",
+    src: "music-02"
+  },
+  {
+    name: "Jung Kook",
+    artist: "JK",
+    img: "music-03",
+    src: "music-03"
+  },
+  {
+    name: "PSY",
+    artist: "Suga ",
+    img: "music-04",
+    src: "music-04"
+  },
+  {
+    name: "JKING",
+    artist: "king",
+    img: "music-05",
+    src: "music-05"
+  },
+  {
+    name: "Charlie Puth ",
+    artist: "Selena Gomez",
+    img: "music-06",
+    src: "music-06"
+  },
+  
+];
